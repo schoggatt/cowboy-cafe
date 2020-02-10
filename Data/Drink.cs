3abd80d98f17d19aa58e71e082a605d23b661708
Sam Hoggatt <schoggatt@users.campus>
2020-02-10 16:57:59 -0600

    ms3

---
+++ Data/Drink.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public abstract class Drink
+    {
+        public abstract double Price { get; }
+
+        public abstract uint Calories { get;  }
+
+        public abstract List<string> Ingredients { get;  }
+
+        public abstract Size Size { get;  }
+
+        public bool Ice { get; set; } = true;
+
+        
+
+
+    }
+}
---
+++ Data/JerkedSoda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class JerkedSoda : Drink
+    {
+        public override double Price => throw new NotImplementedException();
+
+        public override uint Calories => throw new NotImplementedException();
+
+        public override List<string> Ingredients => throw new NotImplementedException();
+
+        public override Size Size => throw new NotImplementedException();
+    }
+}
